88f236435033407da595a5ebc7396f352d32b4fe
ddophi98 <lightcloud98@gmail.com>
2021-07-13 12:51:20 +0900

    타원 <-> 직사각형

---
+++ app/src/main/java/com/example/CS_IU_proto_1/EllipseSize.java
@@ -0,0 +1,18 @@
+package com.example.CS_IU_proto_1;
+
+public class EllipseSize {
+    float r1;
+    float r2;
+    float[] cp;
+    float[] p1;
+    float[] p2;
+
+
+    public EllipseSize(float _r1, float _r2, float[] _cp, float[] _p1, float[] _p2) {
+        r1 = _r1;
+        r2 = _r2;
+        cp = _cp;
+        p1 = _p1;
+        p2 = _p2;
+    }
+}
---
+++ app/src/main/java/com/example/CS_IU_proto_1/RectangleSize.java
@@ -0,0 +1,15 @@
+package com.example.CS_IU_proto_1;
+
+public class RectangleSize {
+    float[] ll;
+    float[] lr;
+    float[] ul;
+    float[] ur;
+
+    public RectangleSize(float[] _ll, float[] _lr, float[] _ul, float[] _ur) {
+        ll = _ll;
+        lr = _lr;
+        ul = _ul;
+        ur = _ur;
+    }
+}
