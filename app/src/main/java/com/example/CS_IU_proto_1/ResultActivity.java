91d72d252cb986234a1def2e1ab332380efde4b0
hegria <awzs159@gmail.com>
2021-08-04 12:08:59 +0900

    아주 간단하게

---
+++ app/src/main/java/com/example/CS_IU_proto_1/ResultActivity.java
@@ -0,0 +1,13 @@
+package com.example.CS_IU_proto_1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
